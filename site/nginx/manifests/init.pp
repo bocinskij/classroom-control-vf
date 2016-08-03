6e63d51953a2c5e247ab2e1a1e6d65139267d97a
bocinskij <jbocinski@bocinski.com>
2016-08-03 15:04:02 -0400

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,24 @@
+class nginx {
+package { 'nginx':
+ensure => present,
+}
+file { 'default.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/nginx/default.conf',
+}
+file { 'nginx.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet:///modules/nginx/nginx.conf',
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+subscribe => File['/
+}
+}
