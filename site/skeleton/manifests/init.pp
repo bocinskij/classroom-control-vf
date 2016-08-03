f10e262e3a1d02bbe2038d0a70278f0f535dd6d0
bocinskij <jbocinski@bocinski.com>
2016-08-03 10:51:29 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,8 +1,14 @@
 class skeleton {
   file { '/etc/skel':
   ensure => 'directory',
+  owner => 'root',
+  group => 'root',
+  mode => '0755',
   }
   file { '/etc/skel/.bashrc':
   ensure => 'file',
-  source => "puppet:///modules/site/skeleton/bashrc",
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
+  source => 'puppet:///modules/skeleton/bashrc',
 }
