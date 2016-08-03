b35676c44eaf21310f5952d1e2f6a3a4e28a24a5
bocinskij <jbocinski@bocinski.com>
2016-08-03 13:36:57 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,13 +1,16 @@
 class memcached {
   package { 'memcached':
   ensure => present,
+  before => File['memcached']
   }
-  file { '/etc/sysconfig/memcached':
+  file { 'memcached':
   ensure => file,
   owner => 'root',
   group => 'root',
   mode => '0644',
+  path => '/etc/sysconfig/memcached',
   source => 'puppet:///modules/memcached/memcached',
+  notify => Service['memcached'],
   }
   service { 'memcached':
   ensure => running,
