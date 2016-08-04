92d1db8a24b03891a0fb86db2ae41148db41f5e5
bocinskij <jbocinski@bocinski.com>
2016-08-04 15:02:13 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,25 +1,51 @@
 class nginx {
+  case $::os['family'] {
+    'redhat', 'debian' : {
+      $owner    = 'root'
+      $group    = 'root'
+      $package  = 'nginx'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = '/var/log/nginx'
+    }
+    'windows' : {
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $package  = 'nginx'
+      $docroot  = 'C:\ProgramData\'
+      $confdir  = '/etc/nginx'
+      $blockdir = "${confdir}/conf.d"
+      $logdir   = "${confdir}/nginx'
+    }
+    default : {
+    fail("Module ${module_name} is not supported on ${os['family']}")
+    }
+  }
+
+
+
   package { 'nginx':
    ensure => present,
   }
   File { 
     ensure => file,
-    owner => 'root',
-    group => 'root',
+    owner => $owner,
+    group => $group,
     mode => '0644',
   }
-  file { '/var/www/':
+  file { $docroot:
     ensure => directory,
   }
-  file { '/var/www/index.html':
+  file { "${docroot}/index.html":
     source => 'puppet:///modules/nginx/index.html',
   }
   file { 'default.conf':
-    path => '/etc/nginx/conf.d/default.conf',
+    path => "${blockdir}/default.conf",
     source => 'puppet:///modules/nginx/default.conf',
   }
   file { 'nginx.conf':
-    path => '/etc/nginx/nginx.conf',
+    path => "${confdir}/nginx.conf",
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   service { 'nginx':
