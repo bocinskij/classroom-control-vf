75c93337699b72c321b36b8771fd3729781ad88c
bocinskij <jbocinski@bocinski.com>
2016-08-04 09:54:36 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,32 +2,24 @@ class nginx {
   package { 'nginx':
    ensure => present,
   }
-  file { '/var/www/':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-  }
-  file { '/var/www/index.html':
+  File { 
     ensure => file,
     owner => 'root',
     group => 'root',
     mode => '0644',
+  }
+  file { '/var/www/':
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
     source => 'puppet:///modules/nginx/index.html',
   }
   file { 'default.conf':
     path => '/etc/nginx/conf.d/default.conf',
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/default.conf',
   }
   file { 'nginx.conf':
     path => '/etc/nginx/nginx.conf',
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   service { 'nginx':
