ebae276d184bef3da92e320864f08fa33d7592a7
bocinskij <jbocinski@bocinski.com>
2016-08-04 09:18:04 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,38 @@
 class nginx {
-package { 'nginx':
-ensure => present,
-}
-file { '/var/www/':
-ensure => directory,
-owner => 'root',
-group => 'root',
-}
-file { '/var/www/index.html':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0644',
-source => 'puppet:///modules/nginx/index.html',
-}
-file { 'default.conf':
-path => '/etc/nginx/conf.d/default.conf',
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0644',
-source => 'puppet:///modules/nginx/default.conf',
-}
-file { 'nginx.conf':
-path => '/etc/nginx/nginx.conf',
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0644',
-source => 'puppet:///modules/nginx/nginx.conf',
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-subscribe => [File['default.conf'],File['nginx.conf']],
-}
+  package { 'nginx':
+   ensure => present,
+  }
+  file { '/var/www/':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { 'default.conf':
+    path => '/etc/nginx/conf.d/default.conf',
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  file { 'nginx.conf':
+    path => '/etc/nginx/nginx.conf',
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [File['default.conf'],File['nginx.conf']],
+  }
 }
